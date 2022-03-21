4922ff13ba3f19550d2b2061cc742a7a417d62e4
thuitaf <faith.thuita@moringaschool.com>
2022-03-21 17:24:32 +0300

    commit tree 0967d0f4e8b2ad379a1032254e6ecedbb7c98d9d

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+// dodger.style.backgroundColor = "#FF69B4";
+// dodger.style.left = "0px";
+
+const dodger = document.getElementById("dodger");
+
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
